33a9c28fb6c6e279f9e9b94f4834e9ac06bb7040
firelop <49453048+firelop@users.noreply.github.com>
2024-01-09 15:05:22 +0100

    NBLNT-11 Ajout d'une template index

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 
 
 app = Flask(__name__)
 
 app.route("/")
 def index():
-    return "<h1>Ouaiiis</h1>"
+    return render_template("index.html")
 
 app.run(host="0.0.0.0", debug=True)
